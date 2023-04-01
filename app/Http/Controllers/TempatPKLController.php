02ab8b91257032eafd57b23d2d4a8b1cb2774aff
nurdinahmadalawiyah <nurdinahmada@gmail.com>
2023-04-01 07:09:09 +0700

    memperbaiki list tempat pkl mahasiswa

--- app/Http/Controllers/TempatPKLController.php
+++ app/Http/Controllers/TempatPKLController.php
@@ -4,13 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Models\TempatPKL;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class TempatPKLController extends Controller
 {
-    public function index() 
+    public function index()
     {
-        $tempat_pkl = TempatPKL::all();
+        $tempat_pkl = DB::table('tempat_pkl')
+        ->join('pengajuan_pkl', 'tempat_pkl.id_pengajuan', '=', 'pengajuan_pkl.id_pengajuan')
+        ->join('mahasiswa', 'pengajuan_pkl.id_mahasiswa', '=', 'mahasiswa.id_mahasiswa')
+        ->leftJoin('pembimbing', 'tempat_pkl.id_pembimbing', '=', 'pembimbing.id_pembimbing')
+        ->join('prodi', 'mahasiswa.prodi', '=', 'prodi.id_prodi')
+        ->select('tempat_pkl.id_tempat_pkl', 'tempat_pkl.id_pengajuan', 'mahasiswa.nama as nama_mahasiswa', 'prodi.nama_prodi', 'mahasiswa.nim', 'pembimbing.nama as nama_pembimbing', 'pembimbing.nik', 'tempat_pkl.konfirmasi_nama_pembimbing', 'tempat_pkl.konfirmasi_nik_pembimbing',)
+        ->get();
 
         return response()->json([
             'status' => 'success',
