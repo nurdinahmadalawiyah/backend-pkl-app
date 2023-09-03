2419b328f4dabaf3380d5af0abfdc50cd0315bba
nurdinahmadalawiyah <nurdinahmada@gmail.com>
2023-09-03 22:03:34 +0700

    add property tahun akademik in list tempat pkl

--- app/Http/Controllers/TempatPKLController.php
+++ app/Http/Controllers/TempatPKLController.php
@@ -13,20 +13,71 @@ class TempatPKLController extends Controller
     public function index()
     {
         $tempat_pkl = DB::table('tempat_pkl')
-        ->join('pengajuan_pkl', 'tempat_pkl.id_pengajuan', '=', 'pengajuan_pkl.id_pengajuan')
-        ->join('mahasiswa', 'pengajuan_pkl.id_mahasiswa', '=', 'mahasiswa.id_mahasiswa')
-        ->leftJoin('pembimbing', 'tempat_pkl.id_pembimbing', '=', 'pembimbing.id_pembimbing')
-        ->join('prodi', 'mahasiswa.prodi', '=', 'prodi.id_prodi')
-        ->select('tempat_pkl.id_tempat_pkl', 'tempat_pkl.id_pengajuan', 'mahasiswa.nama as nama_mahasiswa', 'prodi.nama_prodi', 'mahasiswa.nim','pembimbing.nama as nama_pembimbing', 'pembimbing.nik', 'pengajuan_pkl.*')
-        ->where('mahasiswa.prodi', '=', Auth::user()->id_prodi)
-        ->get();
+            ->join('pengajuan_pkl', 'tempat_pkl.id_pengajuan', '=', 'pengajuan_pkl.id_pengajuan')
+            ->join('mahasiswa', 'pengajuan_pkl.id_mahasiswa', '=', 'mahasiswa.id_mahasiswa')
+            ->leftJoin('pembimbing', 'tempat_pkl.id_pembimbing', '=', 'pembimbing.id_pembimbing')
+            ->join('prodi', 'mahasiswa.prodi', '=', 'prodi.id_prodi')
+            ->select('tempat_pkl.id_tempat_pkl', 'tempat_pkl.id_pengajuan', 'mahasiswa.nama as nama_mahasiswa', 'prodi.nama_prodi', 'mahasiswa.nim', 'pembimbing.nama as nama_pembimbing', 'pembimbing.nik', 'pengajuan_pkl.*')
+            ->where('mahasiswa.prodi', '=', Auth::user()->id_prodi)
+            ->get();
 
+            $tempatPklData = [];
+            foreach ($tempat_pkl as $data) {
+                $tahun_akademik = $this->getTahunAkademik($data->created_at);
+                $tempatPklData[] = [
+                    "id_tempat_pkl" => $data->id_tempat_pkl,
+                    "id_pengajuan" => $data->id_pengajuan,
+                    "nama_mahasiswa" => $data->nama_mahasiswa,
+                    "nama_prodi" => $data->nama_prodi,
+                    "nim" => $data->nim,
+                    "tahun_akademik" => $tahun_akademik,
+                    "nama_pembimbing" => $data->nama_pembimbing,
+                    "nik" => $data->nik,
+                    "id_mahasiswa" => $data->id_mahasiswa,
+                    "nama_perusahaan" => $data->nama_perusahaan,
+                    "alamat_perusahaan" => $data->alamat_perusahaan,
+                    "ditujukan" => $data->ditujukan,
+                    "tanggal_mulai" => $data->tanggal_mulai,
+                    "tanggal_selesai" => $data->tanggal_selesai,
+                    "status" => $data->status,
+                    "surat" => $data->surat,
+                    "created_at" => $data->created_at,
+                    "updated_at" => $data->updated_at,
+                ];
+            }
         return response()->json([
             'status' => 'success',
             'message' => 'Semua Data Konfirmasi Mahasiswa Diterima PKL',
-            'data' => $tempat_pkl,
+            'data' => $tempatPklData,
         ], 200);
     }
+
+    function getTahunAkademik($tahunPKLTimestamp)
+    {
+        // Konversi timestamp string menjadi integer
+        $tahunPKL = strtotime($tahunPKLTimestamp);
+    
+        if ($tahunPKL === false) {
+            return "Format timestamp tidak valid";
+        }
+    
+        // Ambil bulan dari tanggal mahasiswa mulai PKL
+        $bulanPKL = date("n", $tahunPKL);
+        
+        // Jika bulan masuk lebih besar atau sama dengan 9 (September),
+        // maka tahun akademik dimulai pada tahun tahunMasuk,
+        // jika tidak, maka tahun akademik dimulai pada tahun sebelumnya
+        $tahunAkademikAwal = ($bulanPKL >= 9) ? date("Y", $tahunPKL) : (date("Y", $tahunPKL) - 1);
+    
+        // Tahun akademik berakhir selalu 1 tahun setelah tahun akademik dimulai
+        $tahunAkademikAkhir = $tahunAkademikAwal + 1;
+    
+        $tahunAkademik = $tahunAkademikAwal . '/' . $tahunAkademikAkhir;
+    
+        return $tahunAkademik;
+    }
+    
+    
 
     public function store(Request $request)
     {
